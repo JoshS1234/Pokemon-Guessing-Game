3a0c4eee4690e18fe7bf8e3e08ea6625c24a6470
josh <joshua.spence@hotmail.co.uk>
2022-09-24 23:09:47 +0100

    added reminder to filter by generation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import PokemonNameProvider from "./components/PokemonNameProvider.jsx";
+import ChooseGen from "./components/ChooseGen";
 
 function App() {
   return (
     <section>
       <Header />
+      <ChooseGen />
       <PokemonNameProvider />
     </section>
   );
---
+++ src/components/ChooseGen.jsx
@@ -0,0 +1,5 @@
+function ChooseGen() {
+  return <h4>choose generation (add this feature later)</h4>;
+}
+
+export default ChooseGen;
